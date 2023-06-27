348a1e389edd005801d23ec65dc48cbdc0f86b73
rlacjfjin <1151973046@qq.com>
2023-06-27 23:49:01 +0800

    重构1.5.2的例子

--- data.py
+++ data.py
@@ -1,6 +1,56 @@
 from dataclasses import dataclass
+from typing import List, Dict, Union
 
 
-@ dataclass
+@dataclass
 class RefineryData:
+    """
+    Data class for optimization data in crude oil refining.
+    """
+    I: List[Union[str, int]]  # Set of indices for components
+    J: List[Union[str, int]]  # Set of indices for products
+    K: List[Union[str, int]]  # Set of indices for properties
+    Q: Dict[Union[str, int], Dict[Union[str, int], float]]  # Data for property k of component i
+    Pmin: Dict[Union[str, int], Dict[Union[str, int], float]]  # Control specification for property k of product j
+    Pmax: Dict[Union[str, int], Dict[Union[str, int], float]]  # Control specification for property k of product j
+    LX: Dict[Union[str, int], float]  # Lower limit of component i available for use
+    UX: Dict[Union[str, int], float]  # Upper limit of component i available for use
+    LY: Dict[Union[str, int], float]  # Lower limit of blending quantity for product j
+    UY: Dict[Union[str, int], float]  # Upper limit of blending quantity for product j
+    C: Dict[Union[str, int], float]  # Unit cost price of component i
+    Price: Dict[Union[str, int], float]  # Unit selling price of product j
 
+
+ex_data = RefineryData(I=["CM1", "CM2", "CM3", "CM4", "CM5", "CM6"],  # 6种组分
+                       J=["G90", "G93", "G97"],  # 3种汽油
+                       K=["SUL", "RON", "DON", "ARW", "OLE"],  # 5种物性
+                       Price={"G90": 3140, "G93": 3328, "G97": 3517},
+                       C={"CM1": 2356.3, "CM2": 2396.1, "CM3": 2347.2, "CM4": 2470.8, "CM5": 2513.8, "CM6": 3500},
+                       LX={"CM1": 2.835, "CM2": 2.205, "CM3": 4.305, "CM4": 0, "CM5": 0, "CM6": 0},
+                       UX={"CM1": 2.835, "CM2": 2.205, "CM3": 4.305, "CM4": 0.9, "CM5": 5.4, "CM6": 0.45},
+                       LY={"G90": 8, "G93": 6, "G97": 1},
+                       UY={"G90": 1000, "G93": 1000, "G97": 1000},
+                       Q={("CM1", "SUL"): 0.04, ("CM1", "RON"): 89.5, ("CM1", "DON"): 84.25, ("CM1", "ARW"): 12.7,
+                          ("CM1", "OLE"): 39.2,
+                          ("CM2", "SUL"): 0.04, ("CM2", "RON"): 93.2, ("CM2", "DON"): 87.1, ("CM2", "ARW"): 21.4,
+                          ("CM2", "OLE"): 32.4,
+                          ("CM3", "SUL"): 0.04, ("CM3", "RON"): 89.8, ("CM3", "DON"): 84.9, ("CM3", "ARW"): 13,
+                          ("CM3", "OLE"): 39,
+                          ("CM4", "SUL"): 0.001, ("CM4", "RON"): 54.8, ("CM4", "DON"): 50, ("CM4", "ARW"): 0.05,
+                          ("CM4", "OLE"): 0,
+                          ("CM5", "SUL"): 0.001, ("CM5", "RON"): 98.5, ("CM5", "DON"): 93, ("CM5", "ARW"): 70,
+                          ("CM5", "OLE"): 0,
+                          ("CM6", "SUL"): 0.001, ("CM6", "RON"): 114, ("CM6", "DON"): 107, ("CM6", "ARW"): 0,
+                          ("CM6", "OLE"): 0},
+                       Pmin={("G90", "SUL"): 0, ("G90", "RON"): 90.1, ("G90", "DON"): 85.1, ("G90", "OLE"): 0,
+                             ("G90", "ARW"): 0,
+                             ("G93", "SUL"): 0, ("G93", "RON"): 93.1, ("G93", "DON"): 88.1, ("G93", "OLE"): 0,
+                             ("G93", "ARW"): 0,
+                             ("G97", "SUL"): 0, ("G97", "RON"): 97.1, ("G97", "DON"): 92.1, ("G97", "OLE"): 0,
+                             ("G97", "ARW"): 0},
+                       Pmax={("G90", "SUL"): 0.07, ("G90", "RON"): 999, ("G90", "DON"): 999, ("G90", "OLE"): 34,
+                             ("G90", "ARW"): 40,
+                             ("G93", "SUL"): 0.07, ("G93", "RON"): 999, ("G93", "DON"): 999, ("G93", "OLE"): 34,
+                             ("G93", "ARW"): 40,
+                             ("G97", "SUL"): 0.07, ("G97", "RON"): 999, ("G97", "DON"): 999, ("G97", "OLE"): 34,
+                             ("G97", "ARW"): 40})
--- model.py
+++ model.py
@@ -1,12 +1,17 @@
 import pyscipopt as scip
+import pandas as pd
+from data import RefineryData, ex_data
+
+
 class Planning:
     """
     模型参考：1.5.1: 调合计划问题模型
     """
-    def __init__(self,data):
+
+    def __init__(self, data: RefineryData):
         self.data = data
         I, J, K, Q, Pmin, Pmax, LX, UX, LY, UY, C, Price = self._preprocessing()
-
+        self._create_model(I, J, K, Q, Pmin, Pmax, LX, UX, LY, UY, C, Price)
 
     def _preprocessing(self):
         """
@@ -15,7 +20,8 @@ class Planning:
             J: 产品的下标集合
             K: 物性的下标集合
             Q: 组分i的物性k的数据
-            P: 产品j物性k的控制规格
+            Pmin: 产品j物性k的控制规格下限
+            Pmax: 产品j物性k的控制规格上限
             LX: 组分i可供使用的下限
             UX: 组分i可供使用的上限
             LY: 产品j调合量的下限
@@ -23,5 +29,50 @@ class Planning:
             C: 组分i的单位成本价格
             Price: 产品j的单位销售价格
         """
-        I,J,K = self.data["components"],self.data[""]
-        return I, J, K, Q, Pmin, Pmax, LX, UX, LY, UY, C, Price
+        return self.data.I, self.data.J, self.data.K, self.data.Q, \
+            self.data.Pmin, self.data.Pmax, self.data.LX, self.data.UX, \
+            self.data.LY, self.data.UY, self.data.C, self.data.Price
+
+    def _create_model(self, I, J, K, Q, Pmin, Pmax, LX, UX, LY, UY, C, Price):
+        self.model = scip.Model()
+        self.x, self.y, self.b = {}, {}, {}
+        for i in I:
+            self.x[i] = self.model.addVar(name=f"x_{i}", lb=LX[i], ub=UX[i])
+        for j in J:
+            self.y[j] = self.model.addVar(name=f"y_{j}", lb=LY[j], ub=UY[j])
+        for i in I:
+            for j in J:
+                self.b[i, j] = self.model.addVar(name=f"b_{i}_{j}")
+
+        for i in I:
+            self.model.addCons(scip.quicksum(self.b[i, j] for j in J) == self.x[i])
+
+        for j in J:
+            self.model.addCons(scip.quicksum(self.b[i, j] for i in I) == self.y[j])
+
+        for j in J:
+            for k in K:
+                self.model.addCons(scip.quicksum(Q[(i, k)] * self.b[i, j] for i in I) <= Pmax[(j, k)] * self.y[j])
+                self.model.addCons(scip.quicksum(Q[(i, k)] * self.b[i, j] for i in I) >= Pmin[(j, k)] * self.y[j])
+
+        # obj
+        obj = scip.quicksum(-C[i] * self.x[i] for i in I) + scip.quicksum(Price[j] * self.y[j] for j in J)
+        self.model.setObjective(obj, "maximize")
+
+    def solve(self):
+        self.model.optimize()
+        solution = pd.DataFrame(columns=self.data.J, index=self.data.I)
+        for i in self.data.I:
+            for j in self.data.J:
+                solution.loc[i, j] = self.model.getVal(self.b[i, j])
+        result = pd.DataFrame(columns=self.data.J, index=self.data.K)
+        for k in self.data.K:
+            for j in self.data.J:
+                result.loc[k, j] = sum([self.data.Q[(i, k)] * self.model.getVal(self.b[i, j]) for i in self.data.I]) / \
+                                   self.model.getVal(self.y[j])
+        print(solution)
+        print(result)
+
+
+ex_model = Planning(ex_data)
+ex_model.solve()
